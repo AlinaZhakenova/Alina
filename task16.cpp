3eb81853fe6ba51251f74d31f80455b9a24d87b6
AlinaZhakenova <57914729+AlinaZhakenova@users.noreply.github.com>
2019-11-20 19:44:47 +0600

    Add files via upload

---
+++ task16.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double a, b, c;
+	cin >> a >> b >> c;
+	double q, w, e,r;
+	q = a * a;
+	w = b - 3;
+	e = pow(c, 2.0);
+	r=(q + w + e);
+	cout << r << endl;
+	return 0;
+}
