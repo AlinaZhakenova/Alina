251d4a699e77c94ab59c549165f7b86a14077c0d
AlinaZhakenova <57914729+AlinaZhakenova@users.noreply.github.com>
2019-11-20 19:55:57 +0600

    Add files via upload

---
+++ task35.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main(){
+ int a;
+ cin>>a;
+    if (a < 7) {
+        cout<<"YES";
+    }
+    if(a>10){
+        cout<<"NO";
+    }
+    if(a==9){
+        cout<<"ERROR";
+    }
+}
