7e8bac7cdee9b3e20d62408e442329750efe8e0b
AlinaZhakenova <57914729+AlinaZhakenova@users.noreply.github.com>
2019-11-20 19:21:15 +0600

    Add files via upload

---
+++ task5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	string a = "*     *     *", b = " *   * *   * ", c = "  * *   * *  ", v = "   *     *   ";
+	cout << a << endl << b << endl << c << endl << v << endl;
+	return 0;
+}
+
