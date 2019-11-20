2540da40bc1534381cd400b3e78acc031ff6a8cc
AlinaZhakenova <57914729+AlinaZhakenova@users.noreply.github.com>
2019-11-20 19:34:49 +0600

    Add files via upload

---
+++ task10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x = 1.7;
+	cout << (pow(x + 1, 2.0) + 3 * (x + 1)) << endl;
+}
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int x = 3;
+	cout << (pow(x + 1, 2.0) + 3 * (x + 1)) << endl;
+}
