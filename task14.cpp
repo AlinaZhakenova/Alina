34566c8948622a557aaeffa3888af56b5ee71757
AlinaZhakenova <57914729+AlinaZhakenova@users.noreply.github.com>
2019-11-20 19:43:21 +0600

    Add files via upload

---
+++ task14.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	double c, v;
+	c = a + b;
+	v = a * b;
+	cout <<"a + b ="<< c << "    " <<" a * b ="<< v << endl;
+	return 0;
+}
+
