369b021f80e46b6005045932b123f545aa8980a8
AlinaZhakenova <57914729+AlinaZhakenova@users.noreply.github.com>
2019-11-20 19:43:46 +0600

    Add files via upload

---
+++ task15.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int a;
+	cin >> a ;
+	double c, v;
+	c = pow(a,2.0);
+	v = pow(a, 3.0);
+	cout <<"square - "<< c << "    " <<"cube- "<< v << endl;
+	return 0;
+}
