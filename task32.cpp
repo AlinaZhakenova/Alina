648dab4b1e097e19ce7a96d7ac4bdfcc4be1d935
AlinaZhakenova <57914729+AlinaZhakenova@users.noreply.github.com>
2019-11-20 19:55:08 +0600

    Add files via upload

---
+++ task32.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+Using  namespace std;
+int main(){
+    int chastnoe, cifra, x, y=237;
+    cifra=y/100;
+    chastnoe=y%100;
+    x=chastnoe*10+cifra;
+    cout<<x;
+return 0;
+}
