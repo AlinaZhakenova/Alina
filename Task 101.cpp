eca243183d4aeed471c801d6e121697879bee1be
AlinaZhakenova <57914729+AlinaZhakenova@users.noreply.github.com>
2020-04-12 15:42:36 +0600

    Homework1BMI.java

--- Task 101.cpp
+++ Task 101.cpp
@@ -1,10 +1,15 @@
-#include <iostream>
-using namespace std;
-int main (){
-    for(int i=10000; i<=99999; i++){
-        if(i%2==0&&((i/100)%10)%2==1&&((i/10000)%10+(i/1000)%10+(i/100)%10+(i/10)%10+i%10)%4==0){
-            cout<<i<<" ";
-        }
-    }
-    return 0;
+
+import java.util.Scanner;
+
+public class Main {
+ public static void main(String[] args) {
+  Scanner all = new Scanner(System.in);
+  System.out.println ("weight=");
+  float weight = all.nextInt();
+  
+  System.out.println ("height =");
+   float height = all.nextInt();
+
+  System.out.println("BMI="+(weight / (height*height )));
+ }
 }
